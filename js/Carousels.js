8853a2e20f5dcc2051b0affd2f99a00bcc7e19b1
hyesom2642 <kimhyesom@naver.com>
2022-04-22 02:15:51 +0900

    🚧 Divide userGallerySlider mobile, desktop of carousel & Add className for userGallerySlider

--- js/Carousels.js
+++ js/Carousels.js
@@ -13,20 +13,25 @@ const productCarousel = tns({
   //   slideBy: 'page',
 })
 
-const userGallerySlider = tns({
-  container: '.product-user-gallery .user-gallery-slider-list',
+const userGallerySliderMobile = tns({
+  container: '.product-user-gallery.is-mobile .user-gallery-slider-list',
   controls: false,
   gutter: 4,
   mouseDrag: true,
   arrowKeys: true,
-  nav: false,
+  navContainer: '.product-user-gallery.is-mobile .user-gallery-thumbnail-list',
   edgePadding: 16,
   loop: false,
-  responsive: {
-    768: {
-      gutter: 6,
-      controls: true,
-      edgePadding: 75,
-    },
-  },
+})
+
+const userGallerySliderDesktop = tns({
+  container: '.product-user-gallery.is-desktop .user-gallery-slider-list',
+  controls: true,
+  controlsContainer: '.product-user-gallery.is-desktop .user-gallery-controls',
+  gutter: 6,
+  mouseDrag: true,
+  arrowKeys: true,
+  navContainer: '.product-user-gallery.is-desktop .user-gallery-thumbnail-list',
+  edgePadding: 75,
+  loop: false,
 })
