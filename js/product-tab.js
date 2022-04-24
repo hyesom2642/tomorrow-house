af4f8efda75e9c709e674f7528c1905565f237b4
hyesom2642 <kimhyesom@naver.com>
2022-04-24 17:48:31 +0900

    🚧 Implement product-tab-panel to scroll when clicked

--- js/product-tab.js
+++ js/product-tab.js
@@ -5,7 +5,10 @@ const productTabItemButton = productTab.querySelectorAll(
 )
 let currentTabClick = productTab.querySelector('.is-active')
 
-function HandleProductTab() {
+const TOP_HEADER_DESKTOP = 80 + 50 + 54
+const TOP_HEADER_MOBILE = 50 + 40 + 40
+
+function handleProductTab() {
   const productTabItem = this.parentNode
 
   if (currentTabClick !== productTabItem) {
@@ -14,7 +17,22 @@ function HandleProductTab() {
     currentTabClick = productTabItem
   }
 }
+function scrollToPanel() {
+  const tabPanelId = this.parentNode.getAttribute('aria-labelledby')
+  const tabPanel = document.querySelector(`#${tabPanelId}`)
+  console.log(tabPanel)
+
+  const scrollAmount =
+    tabPanel.getBoundingClientRect().top -
+    (window.innerWidth >= 768 ? TOP_HEADER_DESKTOP : TOP_HEADER_MOBILE)
+
+  window.scrollBy({
+    top: scrollAmount,
+    behavior: 'smooth',
+  })
+}
 
 productTabItemButton.forEach((button) => {
-  button.addEventListener('click', HandleProductTab)
+  button.addEventListener('click', handleProductTab)
+  button.addEventListener('click', scrollToPanel)
 })
