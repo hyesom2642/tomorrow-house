e22fec645ef9c39dec15650ce0d224066c9ec337
hyesom2642 <kimhyesom@naver.com>
2023-03-04 02:02:51 +0900

    Implement toggle button of product-review

--- js/review.js
+++ js/review.js
@@ -4,57 +4,23 @@ const helpText = '도움됨'
 const notHelpText = '도움이 돼요'
 
 function helpButtonToggle() {
-  // 1. class: btn-outlined <-> btn-fill-primary
-  // 2. 텍스트내용 도움됨 <-> 도움이돼요
-  // 3. count의 수
-  // this : helpButton
-  const likeButtonState = this.classList.contains('btn-fill-primary') // true, false
+  // 1. class: btn-outlined <-> btn-fill-primary 로 toggle
+  // 2. text 변경 : 도움이 돼요 <-> 도움됨
+  // 3. span의 count수
+  // this: helpButton
 
-  const textElement = this.nextElementSibling
-  const reviewCardFooter = this.parentNode
-
-  if (likeButtonState) {
-    // 활성화 상태 -> 비활성화
-    this.innerHTML = notHelpText
-  } else {
-    // 비활성화 상태 -> 활성화
+  if (this.classList.contains('btn-outlined')) {
     const checkIcon = document.createElement('i')
     checkIcon.classList.add('ic-check')
     checkIcon.setAttribute('aria-hidden', true)
-
     this.innerHTML = helpText
     this.prepend(checkIcon)
-  }
-
-  if (textElement) {
-    // 좋아요 텍스트가 있을 경우
-    const countSpan = textElement.querySelector('span')
-    const count = Number(countSpan.innerHTML.replaceAll(',', ''))
-    let newCount = count
-    if (likeButtonState) {
-      newCount = newCount - 1
-      if (newCount === 0) {
-        reviewCardFooter.removeChild(textElement)
-      } else {
-        countSpan.innerHTML = newCount.toLocaleString()
-      }
-    } else {
-      newCount = newCount + 1
-      countSpan.innerHTML = newCount.toLocaleString()
-    }
   } else {
-    // 좋아요 텍스트가 없을 경우
-    if (!likeButtonState) {
-      // 0명 -> 1명 될 경우
-      const newTextElement = document.createElement('p')
-      newTextElement.innerHTML =
-        '<strong><span>1</span>명</strong>에게 도움이 되었습니다.'
-      reviewCardFooter.appendChild(newTextElement)
-    }
+    this.innerHTML = notHelpText
   }
 
-  this.classList.toggle('btn-fill-primary')
   this.classList.toggle('btn-outlined')
+  this.classList.toggle('btn-fill-primary')
 }
 
 helpButtonList.forEach((button) => {
