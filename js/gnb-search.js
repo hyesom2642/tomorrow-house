5165b415470d17a50212810ce765d651c6656721
hyesom2642 <kimhyesom@naver.com>
2023-03-13 18:27:09 +0900

    Implement delete all button of all search-history-items

--- js/gnb-search.js
+++ js/gnb-search.js
@@ -1,8 +1,18 @@
 const gnbSearch = document.querySelector('.gnb-search')
 const gnbInput = gnbSearch.querySelector('input')
 const gnbSearchHistory = gnbSearch.querySelector('.search-history')
+const gnbSearchHistoryList = gnbSearchHistory.querySelector('ol')
+
+const deleteAllButton = gnbSearchHistory.querySelector(
+  '.search-history-header button'
+)
 
 function openGnbSearch() {
+  if (gnbSearchHistoryList.children.length === 0) {
+    // gnbSearchHistoryList 안의 li가 0개일 경우 실행X
+    return // return: 실행되면 다음에 아무것도 실행X
+  }
+
   if (!gnbSearchHistory.classList.contains('is-active')) {
     // gnbSearchHistory를 활성화 시킬 때
     window.addEventListener('click', closeGnbSearchOnClickOutside)
@@ -21,3 +31,9 @@ function closeGnbSearch() {
   gnbSearchHistory.classList.remove('is-active')
 }
 gnbInput.addEventListener('focus', openGnbSearch)
+
+function deleteAllSearchHistoryItems() {
+  gnbSearchHistoryList.innerHTML = ''
+  closeGnbSearch()
+}
+deleteAllButton.addEventListener('click', deleteAllSearchHistoryItems)
